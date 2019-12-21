1bb29f17c0f5708963454b3075f23306112e9ba0
Satyam <satyamsingh00@gmail.com>
2019-12-21 12:37:59 +0530

    Update create-zookeeper-app-zNodes.sh

--- zookeeper_script/create-zookeeper-app-zNodes.sh
+++ zookeeper_script/create-zookeeper-app-zNodes.sh
@@ -1,8 +1,12 @@
-./zkCli.sh -cmd create /config/spring-apps "application configs"
-./zkCli.sh -cmd create /config/spring-apps/zookeeper-app " zookeeper-app configs"
-./zkCli.sh -cmd create /config/spring-apps/common " common path for configs"
-./zkCli.sh -cmd create /config/spring-apps/common/appliaction " common application configs"
+#These command should be run inside zookeeper-shell (zkCLi).
+#if running these command directly from bin folder of zookeeper, add prefix `./zkCli.sh -cmd` to all the commands.
+#below commands will create the znodes in the zookeeper, required to bring up and run the application
 
-./zkCli.sh -cmd create /config/spring-apps/zookeeper-app/greeting.message "Hello"
-./zkCli.sh -cmd create /config/spring-apps/zookeeper-app/greeting.name "Satyam"
+create /config/spring-apps "application configs"
+create /config/spring-apps/zookeeper-app " zookeeper-app configs"
+create /config/spring-apps/common " common path for configs"
+create /config/spring-apps/common/appliaction " common application configs"
+
+create /config/spring-apps/zookeeper-app/greeting.message "Hello"
+create /config/spring-apps/zookeeper-app/greeting.name "Satyam"
 
