81523a04e0439b0f821ca77992d356958e443917
Satyam <satyamsingh00@gmail.com>
2019-12-21 12:36:05 +0530

    Update change-name-zNode-value.sh

--- zookeeper_script/change-name-zNode-value.sh
+++ zookeeper_script/change-name-zNode-value.sh
@@ -1,1 +1,4 @@
-./zkCli.sh -cmd set /config/spring-apps/zookeeper-app/greeting.name "Singh"
+#This command should be run inside zookeeper-shell (zkCLi).
+#if running this command directly from bin folder of zookeeper, append `./zkCli.sh -cmd` to the command.
+#Change value of property `greeting.name` to Singh in Zookeeper.
+set /config/spring-apps/zookeeper-app/greeting.name "Singh"
