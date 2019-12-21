f9394e8465d4d4859dc634d994fdeaa8facd7c6e
satyam.s.singh <satyam.s.singh@oracle.com>
2019-12-21 12:30:10 +0530

    removed zk scripts from root

--- change-name-zNode-value.sh
+++
@@ -1,1 +0,0 @@
-./zkCli.sh -cmd set /config/spring-apps/zookeeper-app/greeting.name "Singh"
--- create-zookeeper-app-zNodes.sh
+++
@@ -1,8 +0,0 @@
-./zkCli.sh -cmd create /config/spring-apps "application configs"
-./zkCli.sh -cmd create /config/spring-apps/zookeeper-app " zookeeper-app configs"
-./zkCli.sh -cmd create /config/spring-apps/common " common path for configs"
-./zkCli.sh -cmd create /config/spring-apps/common/appliaction " common application configs"
-
-./zkCli.sh -cmd create /config/spring-apps/zookeeper-app/greeting.message "Hello"
-./zkCli.sh -cmd create /config/spring-apps/zookeeper-app/greeting.name "Satyam"
-
