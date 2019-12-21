80ebaaa3d57aa0f6d5cf18f3bdd26c41eb450b9a
satyam.s.singh <satyam.s.singh@oracle.com>
2019-12-21 10:56:14 +0530

    initial commit for zookeeper config app

---
+++ src/main/java/com/sample/zookeeper/ZookeeperGreetingsApplication.java
@@ -0,0 +1,13 @@
+package com.sample.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperGreetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperGreetingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/zookeeper/ZookeeperGreetingsApplicationTest.java
@@ -0,0 +1,16 @@
+package com.sample.zookeeper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Satyam Singh
+ */
+public class ZookeeperGreetingsApplicationTest {
+
+    @Test
+    public void main() {
+        ZookeeperGreetingsApplication.main(new String[] {});
+    }
+}
